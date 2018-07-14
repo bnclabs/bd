e614d18b2df6a3cc80fff1aaf3f4fc0caafcf11a
prataprc <prataprc@gmail.com>
2018-07-14 20:57:36 +0530

    dev: JSON, implement array and object parsing.

--- src/json.rs
+++ src/json.rs
@@ -1,10 +1,12 @@
-//use std::collections::HashMap;
+use std::collections::HashMap;
 use std::result;
 
 pub type Result<T> = result::Result<T,Error>;
 
 pub enum Error {
     InvalidToken(usize, usize, usize),
+    DuplicateKey(usize, usize, usize),
+    KeyIsNotString(usize, usize, usize),
     MissingToken,
 }
 
@@ -15,10 +17,9 @@ impl Lex {
         Lex{off, row, col}
     }
 
-    fn update(&mut self, off: usize, row: usize, col: usize) {
-        self.off = off;
-        self.row = row;
-        self.col = col;
+    fn incr_col(&mut self, i: usize) {
+        self.off += i;
+        self.col += i;
     }
 }
 
@@ -34,32 +35,65 @@ impl JsonBuf {
 
     pub fn set<T>(&mut self, text: &T) where T: AsRef<str> + ?Sized {
         self.inner.clear();
-        self.inner.push_str(text.as_ref())
+        self.inner.push_str(text.as_ref());
+    }
+
+    pub fn append<T>(&mut self, text: &T) where T: AsRef<str> + ?Sized {
+        self.inner.push_str(text.as_ref());
     }
 
     pub fn sanitize(&mut self) -> Result<Json> {
-        use ::json::Error::{InvalidToken, MissingToken};
+        use ::json::Error::{MissingToken};
 
         let mut lex = Lex::new(0_usize, 1_usize, 1_usize);
-
         if self.inner.len() == 0 {
             return Err(MissingToken)
         }
+        let val = self.sanitize_value(&mut lex)?;
+        self.inner = self.inner[lex.off..].to_string(); // remaining text.
+        Ok(val)
+    }
 
-        self.parse_whitespace(&mut lex);
+    pub fn parse(&mut self) -> Json {
+        let mut lex = Lex::new(0_usize, 1_usize, 1_usize);
+
+        let val = self.parse_value(&mut lex);
+        self.inner = self.inner[lex.off..].to_string(); // remaining text.
+        val
+    }
+
+    fn sanitize_value(&mut self, lex: &mut Lex) -> Result<Json> {
+        use ::json::Error::{InvalidToken};
+
+        self.parse_whitespace(lex);
         let val = match &self.inner[lex.off..].chars().nth(0).unwrap() {
-            'n' => self.sanitize_null(&mut lex)?,
-            't' => self.sanitize_true(&mut lex)?,
-            'f' => self.sanitize_false(&mut lex)?,
-            '0'..='9'|'+'|'-'|'.'|'e'|'E' => self.sanitize_num(&mut lex)?,
-            '"' => self.sanitize_string(&mut lex)?,
-            //'[' => validate_array(lex)),
-            //'{' => Some(parse_object(lex)),
+            'n' => self.sanitize_null(lex)?,
+            't' => self.sanitize_true(lex)?,
+            'f' => self.sanitize_false(lex)?,
+            '0'..='9'|'+'|'-'|'.'|'e'|'E' => self.sanitize_num(lex)?,
+            '"' => self.sanitize_string(lex)?,
+            '[' => self.sanitize_array(lex)?,
+            '{' => self.sanitize_object(lex)?,
             _ => return Err(InvalidToken(lex.off, lex.row, lex.col)),
         };
 
-        self.inner = self.inner[lex.off..].to_string(); // remaining text.
         Ok(val)
+    }
+
+    fn parse_value(&mut self, lex: &mut Lex) -> Json {
+        self.parse_whitespace(lex);
+        let val = match &self.inner[lex.off..].chars().nth(0).unwrap() {
+            'n' => self.parse_null(lex),
+            't' => self.parse_true(lex),
+            'f' => self.parse_false(lex),
+            '0'..='9'|'+'|'-'|'.'|'e'|'E' => self.parse_num(lex),
+            '"' => self.parse_string(lex),
+            '[' => self.parse_array(lex),
+            '{' => self.parse_object(lex),
+            _ => panic!("invalid token")
+        };
+
+        val
     }
 
     fn sanitize_null(&self, lex: &mut Lex) -> Result<Json> {
@@ -67,8 +101,7 @@ impl JsonBuf {
 
         let text = &self.inner[lex.off..];
         if text.len() == 4 && &text[0..4] == "null" {
-            let (off, row, col) = (lex.off+4, lex.row, lex.col+4);
-            lex.update(off, row, col);
+            lex.incr_col(4);
             Ok(Json::Null)
         } else {
             Err(InvalidToken(lex.off, lex.row, lex.col))
@@ -76,8 +109,7 @@ impl JsonBuf {
     }
 
     fn parse_null(&self, lex: &mut Lex) -> Json {
-        let (off, row, col) = (lex.off+4, lex.row, lex.col+4);
-        lex.update(off, row, col);
+        lex.incr_col(4);
         Json::Null
     }
 
@@ -86,8 +118,7 @@ impl JsonBuf {
 
         let text = &self.inner[lex.off..];
         if text.len() == 4 && &text[..4] == "true" {
-            let (off, row, col) = (lex.off+4, lex.row, lex.col+4);
-            lex.update(off, row, col);
+            lex.incr_col(4);
             Ok(Json::Bool(true))
         } else {
             Err(InvalidToken(lex.off, lex.row, lex.col))
@@ -95,8 +126,7 @@ impl JsonBuf {
     }
 
     fn parse_true(&self, lex: &mut Lex) -> Json {
-        let (off, row, col) = (lex.off+4, lex.row, lex.col+4);
-        lex.update(off, row, col);
+        lex.incr_col(4);
         Json::Bool(true)
     }
 
@@ -105,8 +135,7 @@ impl JsonBuf {
 
         let text = &self.inner[lex.off..];
         if text.len() == 5 && &text[..5] == "false" {
-            let (off, row, col) = (lex.off+5, lex.row, lex.col+5);
-            lex.update(off, row, col);
+            lex.incr_col(5);
             Ok(Json::Bool(false))
         } else {
             Err(InvalidToken(lex.off, lex.row, lex.col))
@@ -114,8 +143,7 @@ impl JsonBuf {
     }
 
     fn parse_false(&self, lex: &mut Lex) -> Json {
-        let (off, row, col) = (lex.off+5, lex.row, lex.col+5);
-        lex.update(off, row, col);
+        lex.incr_col(5);
         Json::Bool(false)
     }
 
@@ -129,12 +157,10 @@ impl JsonBuf {
                 _ => (),
             }
             if let Ok(num) = text[..i].parse::<i128>() {
-                let (off, row, col) = (lex.off+i, lex.row, lex.col+i);
-                lex.update(off, row, col);
+                lex.incr_col(i);
                 return Ok(Json::Integer(num))
             } else if let Ok(num) = text[..i].parse::<f64>() {
-                let (off, row, col) = (lex.off+i, lex.row, lex.col+i);
-                lex.update(off, row, col);
+                lex.incr_col(i);
                 return Ok(Json::Float(num))
             }
             return Err(InvalidToken(lex.off, lex.row, lex.col))
@@ -150,12 +176,10 @@ impl JsonBuf {
                 _ => (),
             }
             if let Ok(num) = text[..i].parse::<i128>() {
-                let (off, row, col) = (lex.off+i, lex.row, lex.col+i);
-                lex.update(off, row, col);
+                lex.incr_col(i);
                 return Json::Integer(num)
             } else if let Ok(num) = text[..i].parse::<f64>() {
-                let (off, row, col) = (lex.off+i, lex.row, lex.col+i);
-                lex.update(off, row, col);
+                lex.incr_col(i);
                 return Json::Float(num)
             }
             break
@@ -204,8 +228,7 @@ impl JsonBuf {
 
             }
             if ch == '"' { // exit
-                let (off, row, col) = (lex.off+i, lex.row, lex.col+i);
-                lex.update(off, row, col);
+                lex.incr_col(i);
                 return Ok(Json::String(s))
             }
             match ch as i32 { // normal character
@@ -214,6 +237,110 @@ impl JsonBuf {
             }
         }
         Err(InvalidToken(self.inner.len(), lex.row, lex.col))
+    }
+
+    fn parse_string(&mut self, lex: &mut Lex) -> Json {
+        if let Ok(val) = self.sanitize_string(lex) {
+            return val
+        }
+        panic!("invalid string")
+    }
+
+    fn sanitize_array(&mut self, lex: &mut Lex) -> Result<Json> {
+        use ::json::Error::InvalidToken;
+
+        lex.incr_col(1); // skip '['
+
+        let mut array = Vec::new();
+        loop {
+            self.parse_whitespace(lex);
+            if (&self.inner[lex.off..]).len() == 0 {
+                break Err(InvalidToken(lex.off, lex.row, lex.col))
+            } else if (&self.inner[lex.off..]).as_bytes()[0] == b']' { // exit
+                lex.incr_col(1);
+                break Ok(Json::Array(array))
+            }
+            array.push(self.sanitize_value(lex)?);
+        }
+    }
+
+    fn parse_array(&mut self, lex: &mut Lex) -> Json {
+        lex.incr_col(1); // skip '['
+
+        let mut array = Vec::new();
+        loop {
+            self.parse_whitespace(lex);
+            if (&self.inner[lex.off..]).as_bytes()[0] == b']' { // exit
+                lex.incr_col(1);
+                break Json::Array(array)
+            }
+            array.push(self.parse_value(lex));
+        }
+    }
+
+    fn sanitize_object(&mut self, lex: &mut Lex) -> Result<Json> {
+        use ::json::Error::{InvalidToken, DuplicateKey, KeyIsNotString};
+
+        lex.incr_col(1); // skip '{'
+
+        let mut map = HashMap::new();
+        loop {
+            // key
+            self.parse_whitespace(lex);
+            let key = self.sanitize_string(lex)?;
+            if let Json::String(_) = key {
+                break Err(KeyIsNotString(lex.off, lex.row, lex.col))
+            }
+            let key = key.string();
+            // colon
+            self.parse_whitespace(lex);
+            if (&self.inner[lex.off..]).len() == 0 {
+                break Err(InvalidToken(lex.off, lex.row, lex.col))
+            } else if (&self.inner[lex.off..]).as_bytes()[0] != b':' {
+                break Err(InvalidToken(lex.off, lex.row, lex.col))
+            }
+            lex.incr_col(1); // skip ':'
+            // value
+            self.parse_whitespace(lex);
+            let value = self.sanitize_value(lex)?;
+            // is exit
+            self.parse_whitespace(lex);
+            if (&self.inner[lex.off..]).len() == 0 {
+                break Err(InvalidToken(lex.off, lex.row, lex.col))
+            } else if (&self.inner[lex.off..]).as_bytes()[0] == b'}' { // exit
+                lex.incr_col(1);
+                break Ok(Json::Object(map))
+            }
+            if let Some(_) = map.insert(key, value) {
+                break Err(DuplicateKey(lex.off, lex.row, lex.col))
+            }
+        }
+    }
+
+    fn parse_object(&mut self, lex: &mut Lex) -> Json {
+        lex.incr_col(1); // skip '{'
+
+        let mut map = HashMap::new();
+        loop {
+            // key
+            self.parse_whitespace(lex);
+            let key = self.parse_string(lex).string();
+            // colon
+            self.parse_whitespace(lex);
+            lex.incr_col(1); // skip ':'
+            // value
+            self.parse_whitespace(lex);
+            let value = self.parse_value(lex);
+            // is exit
+            self.parse_whitespace(lex);
+            if (&self.inner[lex.off..]).len() == 0 {
+                panic!("invalid token");
+            } else if (&self.inner[lex.off..]).as_bytes()[0] == b'}' { // exit
+                lex.incr_col(1);
+                break Json::Object(map)
+            }
+            map.insert(key, value);
+        }
     }
 
     fn parse_whitespace(&self, lex: &mut Lex) {
@@ -247,104 +374,28 @@ pub enum Json {
     Integer(i128),
     Float(f64),
     String(String),
-    //Array(Vec<Json>),
-    //Object(HashMap<String, Json>),
+    Array(Vec<Json>),
+    Object(HashMap<String, Json>),
 }
 
-//pub fn parse<'a>(text: &'a str) -> Option<Result<'a>> {
-//    let mut lex = Lex{text, off: 0, row: 1, col: 1};
-//    parse_value(&mut lex)
-//}
-//
-//fn parse_value<'a>(lex: &'a mut Lex) -> Option<Result<'a>> {
-//    use ::json::Error::InvalidToken;
-//
-//    parse_whitespace(lex);
-//
-//    if lex.text.len() > 0 {
-//        match lex.text.chars().nth(0).unwrap() {
-//            'n' => Some(parse_null(lex)),
-//            't' => Some(parse_bool(lex)),
-//            'f' => Some(parse_bool(lex)),
-//            '0'..='9'|'+'|'-'|'.'|'e'|'E' => Some(parse_number(lex)),
-//            '"' => Some(parse_string(lex)),
-//            '[' => Some(parse_array(lex)),
-//            //'{' => Some(parse_object(lex)),
-//            _ => Some(Err(InvalidToken(lex.off, lex.row, lex.col))),
-//        }
-//
-//    } else {
-//        None
-//    }
-//}
-//
-//fn parse_string<'a>(lex: &'a mut Lex) -> Result<'a> {
-//    use ::json::Error::InvalidToken;
-//
-//    let s = String::new();
-//    let esc = false;
-//    let u = String::new();
-//    let chars = lex.text.chars().enumerate();
-//
-//    chars.next(); // skip the opening quote
-//
-//    for (i, ch) in lex.text.chars().enumerate() {
-//        if esc { // escaped
-//            match ch {
-//                'b'  => {esc = false; s.push(8 as char)},
-//                't'  => {esc = false; s.push(9 as char)},
-//                'n'  => {esc = false; s.push(10 as char)},
-//                'f'  => {esc = false; s.push(12 as char)},
-//                'r'  => {esc = false; s.push(13 as char)},
-//                '"'  => {esc = false; s.push(34 as char)},
-//                '/'  => {esc = false; s.push(47 as char)},
-//                '\\' => {esc = false; s.push(92 as char)},
-//                'u'  => {u.push_str("\\u{")},
-//                _ => return Err(InvalidToken(lex.off+i, lex.row, lex.col))
-//            }
-//            continue
-//
-//        } else if u.len() > 0 { // unicode escaped
-//            match ch {
-//                '0'..='9' | 'a'..='f' | 'A'..='F' if u.len() < 7 => u.push(ch),
-//                _ => return Err(InvalidToken(lex.off+i, lex.row, lex.col))
-//            };
-//            if u.len() == 7 {
-//                u.push('}');
-//                s.push_str(&u);
-//                u.clear();
-//            }
-//            continue
-//
-//        } else if ch == '"' {
-//            lex.update(&lex.text[i..], lex.off+i, lex.row, lex.col+i);
-//            return Ok(Json::String(s))
-//        }
-//        match ch as i32 {
-//            0x20 | 0x21 | 0x23..=0x5B | 0x5D..=0x10FFFF => s.push(ch as char),
-//            _ => return Err(InvalidToken(lex.off+i, lex.row, lex.col)),
-//        }
-//    }
-//    Err(InvalidToken(lex.text.len(), lex.row, lex.col))
-//}
-//
-//fn parse_array<'a>(lex: &'a mut Lex) -> Result<'a> {
-//    use ::json::Error::InvalidToken;
-//
-//    lex.update(&lex.text[1..], lex.off+1, lex.row, lex.col+1);
-//
-//    let array = Vec::new();
-//    loop {
-//        parse_whitespace(lex);
-//        if lex.text.len() == 0 {
-//            break Err(InvalidToken(lex.off, lex.row, lex.col))
-//        } else if lex.text.as_bytes()[0] == b']' { // exit array
-//            break Ok(Json::Array(array))
-//        } else if let Some(value) = parse_value(lex) {
-//            array.push(value?);
-//            continue
-//        } else {
-//            break Err(InvalidToken(lex.off, lex.row, lex.col))
-//        }
-//    }
-//}
+impl Json {
+    pub fn bool(self) -> bool {
+        use self::Json::{Bool};
+        match self { Bool(val) => val, _ => panic!("Json is not boolean") }
+    }
+
+    pub fn integer(self) -> i128 {
+        use self::Json::{Integer};
+        match self { Integer(val) => val, _ => panic!("Json is not integer") }
+    }
+
+    pub fn float(self) -> f64 {
+        use self::Json::{Float};
+        match self { Float(val) => val, _ => panic!("Json is not float") }
+    }
+
+    pub fn string(self) -> String {
+        use self::Json::{String};
+        match self { String(val) => val, _ => panic!("Json is not string") }
+    }
+}
