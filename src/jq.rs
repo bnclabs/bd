7db5d910704a8c17a7a2bc628d6f0c4cd4c00c20
prataprc <prataprc@gmail.com>
2018-07-22 11:36:44 +0530

    jq: dev commit.

--- src/jq.rs
+++ src/jq.rs
@@ -1,4 +1,43 @@
-trait Thunk {
+struct PathExpr(String);
+
+enum Thunk<I> where I: Iterator<Item=Json> {
+    Source(I),
+    Lookup(PathExpr, Thunk),
+}
+
+impl Thunk<I> where I: Iterator<Item=Json> {
+    fn handle_source(&self, iter: I) -> I {
+        return iter
+    }
+
+    fn handle_lookup(&self, path_expr: PathExpr, operand: Thunk) -> I {
+        let items = Vec::new();
+        for item in operand() {
+        }
+    }
+}
+
+impl<I> Fn<()> for Thunk<I> where I: Iterator<Item=Json> {
+    extern "rust-call" fn call(&self, args: ()) -> Self::Output {
+        match self {
+            Source(iter) => self.handle_source(iter),
+            Lookup(path_expr, thunk) => self.handle_lookup(path_expr, thunk),
+        }
+    }
+}
+
+impl<I> FnMut<()> for Thunk<I> where I: Iterator<Item=Json> {
+    extern "rust-call" fn call_mut(&mut self, args: ()) -> Self::Output {
+        self.call()
+    }
+}
+
+impl<I> FnOnce<()> for Thunk<I> where I: Iterator<Item=Json> {
+    type Output=I;
+
+    extern "rust-call" fn call_mut(self, args: ()) -> Self::Output {
+        self.call()
+    }
 }
 
 pub fn compile(program: &str) -> Thunk {
